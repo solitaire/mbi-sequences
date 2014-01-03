0935bd973aca965154326cd025fd81f0115996f4
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2014-01-03 18:28:40 +0100

    Add Mapping for GAP

--- src/nw/structures/Alphabet.scala
+++ src/nw/structures/Alphabet.scala
@@ -3,7 +3,7 @@ package nw.structures
 object Alphabet extends Enumeration {
 	val A, G, C, T, GAP = Value
 
-  val mapping = Map('A' -> A, 'G' -> G, 'C' -> C, 'T' -> T)
+  val mapping = Map('A' -> A, 'G' -> G, 'C' -> C, 'T' -> T, '-' -> GAP)
 
   def apply(opt: Option[Char]): Value = opt.map(c => mapping(c)).getOrElse(GAP)
 }
