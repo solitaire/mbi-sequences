33e85c8802332eda8739360ea2ba673ed60cc76c
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2014-01-03 20:09:15 +0100

    Begin implementing CLI

--- src/mbi/sequences/App.scala
+++ src/mbi/sequences/App.scala
@@ -6,5 +6,41 @@ package mbi.sequences
  */
 object App extends scala.App {
   println("mbi.sequences application has run")
-  // TODO read sequences and similarity matrix from files
+  val usage =
+    """ Usage:
+      | -seq <path to first sequence> <path to second sequence> <path to third sequence> -sm <path to similarity matrix>
+      |
+      | Example
+      | -seq data/s1 data/s2 data/s3 -sm data/similarity-matrix >> results
+    """.stripMargin
+
+  def nextOption(optionsMap: Map[String, Any], remainingArgs: List[String]): Map[String, Any] = {
+    remainingArgs match {
+      case "-seq" :: p1 :: p2 :: p3 :: tail => nextOption(optionsMap ++ Map("-seq" -> List(p1, p2, p3)), tail)
+      case "-sm" :: p :: tail => nextOption(optionsMap ++ Map("-sm" -> p), tail)
+      case rest => optionsMap
+    }
+  }
+
+  if(args.length < 6) println(usage)
+  else {
+    val optionsMap: Map[String, Any] = nextOption(Map.empty, args.toList)
+    if (!(optionsMap.contains("-seq") && optionsMap("-seq").asInstanceOf[List[String]].size == 3)) println(
+      """
+        |Wrong usage of -seq
+        |
+        |example: -seq sequence1 ../some/dir/seq2 data/seq3
+      """.stripMargin)
+
+    if (!(optionsMap.contains("-sm") && optionsMap("-sm").asInstanceOf[String].size > 0)) println(
+      """
+        |Wrong usage of -sm
+        |
+        |example: -sm path/to/similarity-matrix
+      """.stripMargin)
+
+    println(optionsMap)
+
+  }
+
 }
